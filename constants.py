c596f3d2ee7f71445e7ec71a4b143f50c32cc009
thesersaer <sersaer@gmail.com>
2024-10-13 16:04:57 +0200

    Game constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+# Physics engine
+PHYSICS_TIMESTEP_MS = 3
+
+# Video
+DISPLAY_RESOLUTION = (1280, 720)
+RENDER_FRAME_RATE = 60
+GRID_SIZE = 32
+
+# Game-related
+PLAYER_Y_RATIO = 5/4
+GRID_MEASURE = 10      # Correlation between grid size and distance units it represents physically, in light-seconds
+PLAYER_SIZE = (4, 6)
