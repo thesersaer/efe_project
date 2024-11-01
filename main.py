40876a5da40199ab770a8c0444a0c51917a12793
Sergio Sánchez <sersaer@gmail.com>
2024-11-01 18:32:20 +0100

    Contract main

--- main.py
+++ main.py
@@ -1,38 +1,4 @@
-import typing
-import pygame
-import constants
+from game import engineclass as en
 
-class Game:
-    def __init__(self):
-        pygame.init()
-        self._clock = Clock()
-        self._screen = Screen()
-        self.is_running = False
-
-    def start(self):
-        self.is_running = True
-
-    def stop(self):
-        self.is_running = False
-
-    def loop(self):
-        while self.is_running:
-            self._clock.tick()
-            # Event processing
-            # Physics update / catch-up
-            # Rendering
-
-
-class Clock:
-    def __init__(self):
-        self._clock = pygame.time.Clock()
-        self._lag = 0
-
-    def tick(self) -> int:
-        elapsed = self._clock.tick(constants.RENDER_FRAME_RATE)
-        self._lag += elapsed
-        return elapsed
-
-class Screen:
-    def __init__(self):
-        self._screen = pygame.display.set_mode(constants.DISPLAY_RESOLUTION)
+game = en.GameEngine()
+game.start()
