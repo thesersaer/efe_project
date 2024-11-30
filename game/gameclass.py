01267db5d75e687027e2da342c5742821f0ec00a
thesersaer <sersaer@gmail.com>
2024-11-30 11:50:41 +0100

    Implement SceneManager class

--- game/gameclass.py
+++ game/gameclass.py
@@ -5,34 +5,24 @@ from game.gameobject import Actor
 
 class Game:
     def __init__(self):
-        self.scenes = {}
-        self._active_scene = None
+        self.scene_manager = SceneManager()
         self.map = Map()
 
-        map_obj = Actor(self.map.group)
-        map_obj.maths.velocity.update(0, -0.2, -0.3)
-        self.map.add_object(map_obj)
+        # map_obj = Actor(self.map.group)
+        # map_obj.maths.velocity.update(0, -0.2, -0.3)
+        # self.map.add_object(map_obj)
+        # self.scene_manager.add('Ship')
+        # self.scene_manager.switch('Ship')
 
-        self.scenes.update({'ship': scene.Ship()})
-        self.set_active_scene('ship')
-        self.active_scene.map_view.load_seek_group(self.map.group)
-
-
-    @property
-    def active_scene(self):
-        return self.scenes[self._active_scene]
-
-    def set_active_scene(self, scene_key):
-        if scene_key in self.scenes.keys():
-            self._active_scene = scene_key
 
     def update(self):
-        self.scenes[self._active_scene].update()
+        if self.scene_manager.scene:
+            self.scene_manager.scene.update()
 
     def draw(self, display):
-
-        self.active_scene.draw()
-        display.blit(self.active_scene.surface, (0, 0))
+        if self.scene_manager.scene:
+            self.scene_manager.scene.draw()
+            display.blit(self.scene_manager.scene.surface, (0, 0))
 
 
 class Map(scene.Scene):
@@ -45,3 +35,39 @@ class Map(scene.Scene):
     def add_object(self, obj: Object):
 
         self.group.add(obj)
+
+
+class SceneManager:
+
+    def __init__(self):
+
+        self.scenes = {}
+        self.scene = None
+
+    @staticmethod
+    def exists(scene_key: str):
+        return scene_key in scene.__all__
+
+
+    def add(self, scene_key: str):
+
+        if self.exists(scene_key) and scene_key not in self.scenes.keys():
+
+            self.scenes[scene_key] = scene.__dict__.get(scene_key)()
+
+
+    def remove(self, scene_key: str):
+
+        if self.exists(scene_key) and scene_key in self.scenes.keys():
+
+            self.scenes.pop(scene_key)
+
+
+    def switch(self, scene_key: str):
+
+        if self.exists(scene_key) and scene_key in self.scenes.keys():
+            if self.scene:
+                self.scene.exit()
+
+            self.scene = self.scenes[scene_key]
+            self.scene.enter()
